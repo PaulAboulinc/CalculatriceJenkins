36fd8d44e8f0669986ca518e9a00e9dfc464dced
Paul Aboulinc <paul.aboulinc@outlook.fr>
2021-01-20 11:30:24 +0100

    fix sonarqube

--- src/main/java/Calculatrice/Calculatrice.java
+++ src/main/java/Calculatrice/Calculatrice.java
@@ -1,8 +1,13 @@
+package Calculatrice;
+
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
 public class Calculatrice {
+    private Calculatrice() {
+    }
+
     public static String calculate(String calculationString) throws ScriptException {
         ScriptEngineManager script = new ScriptEngineManager();
         ScriptEngine eng = script.getEngineByName("JavaScript");
